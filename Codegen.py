c11e1222724651c7b2d9fa67ac933837f492fd38
Kian Bahadori <79361265+Kian-bhd@users.noreply.github.com>
2024-02-05 02:22:12 +0330

    Late night commit (added base for recursive function calls)

--- Codegen.py
+++ Codegen.py
@@ -1,21 +1,27 @@
 class Codegen:
     def __init__(self):
         self.SS = []
-        self.PB = []
+        self.PB = ['JP, ?, , ']
         self.BS = []
-        self.i = 0
+        self.RS = []
+        self.i = 1
         self.cur_type = 'void'
+        self.cur_scope = 0
         self.addr = 100
         self.temp_addr = 500
-        self.symbol_table = {}  # {'var1'=[type1, addr1, size], 'var2'=[type2, addr2]}
+        self.symbol_table = []  # [['var1', type1, addr1, size, scope], ['var2', type2, addr2, size, scope], ['fun1', 'func', [return_value, argument_list, return_address, cur_i], argcnt, scope]]
         self.op_dict = {'+': 'ADD', '-': 'SUB', '*': 'MULT', '<': 'LT', '==': 'EQ'}
+        self.main_addr = None
 
     def code_gen(self, action, lexeme):
         print(action, ':', lexeme, ':', self.SS)
         if action == '#set_id_type':
             self.cur_type = lexeme
+        elif action == '#pid_dec':
+            addr = self.find_addr(lexeme, True)
+            self.SS.append(addr)
         elif action == '#pid':
-            addr = self.find_addr(lexeme)
+            addr = self.find_addr(lexeme, False)
             self.SS.append(addr)
         elif action == '#assign':
             self.insert_code('ASSIGN', self.SS[-1], self.SS[-2])
@@ -24,14 +30,15 @@ class Codegen:
             self.SS.append(lexeme)
         elif action == '#set_index':
             self.update_index(self.SS[-2], self.SS[-1])
-            self.addr += 4 * (int(self.SS[-1]) - 1)
+            self.temp_addr += 4 * (int(self.SS[-1]) - 1)
             self.SS.pop()
             self.SS.pop()
         elif action == '#pnum':
             self.SS.append(f'#{lexeme}')
         elif action == '#p_array_addr':
             idx = self.SS.pop()
-            idx = idx[1:]
+            if str(idx).startswith('#'):
+                idx = idx[1:]
             addr = self.SS.pop()
             self.SS.append((int(addr) + 4 * (int(idx))))
         elif action == '#push_op':
@@ -85,6 +92,46 @@ class Codegen:
             while break_addr != '<':
                 self.PB[break_addr] = f'JP, {self.i}, , '
                 break_addr = self.BS.pop()
+        elif action == '#scope_plus':
+            self.cur_scope += 1
+        elif action == '#scope_minus':
+            for record in self.symbol_table[::-1]:
+                print(record)
+                if record[4] == self.cur_scope:
+                    del self.symbol_table[-1]
+            self.cur_scope -= 1
+        elif action == '#func_init':
+            self.symbol_table.append('>>')
+        elif action == '#func_symbol':
+            args_idx = self.symbol_table.index('>>')
+            args_list = self.symbol_table[args_idx + 1:]
+            argcnt = len(args_list)
+            self.symbol_table.pop(args_idx)
+            return_val = self.get_temp()
+            return_addr = self.get_temp()
+            self.SS.append(return_val)
+            self.SS.append(return_addr)
+            func_name = self.symbol_table[args_idx - 1][0]
+            self.symbol_table.append([func_name, 'func', [return_val, args_list, return_addr, self.i], argcnt, self.cur_scope])
+            if func_name == 'main':
+                self.main_addr = self.i
+                self.PB[0] = f'JP, {self.main_addr}, , '
+        elif action == '#new_rs':
+            self.RS.append('<')
+        elif action == '#return':
+            self.RS.append((self.i, self.SS.pop()))
+            self.insert_code('ASSIGN', '?', '?', )
+            self.insert_code('JP', '?')
+        elif action == '#end_rs':
+            return_addr = self.SS.pop()
+            return_value = self.SS.pop()
+            ret_elem = self.RS.pop()
+            while ret_elem != '<':
+                self.PB[ret_elem[0]] = f'ASSIGN, {ret_elem[1]}, {return_value}'
+                self.PB[ret_elem[0] + 1] = f'JP, @{return_addr}, ,'
+                ret_elem = self.RS.pop()
+        elif action == '#implicit_zero_return':
+            self.SS.append('#0')
 
     def get_temp(self):
         tmp = self.temp_addr
@@ -97,20 +144,23 @@ class Codegen:
         self.i += 1
 
     def update_index(self, addr, idx):
-        for var in self.symbol_table.keys():
-            if self.symbol_table[var][1] == addr:
-                self.symbol_table[var][2] = idx
+        for var in self.symbol_table[::-1]:
+            if var[2] == addr:
+                var[3] = idx
                 print('Symbol Table:')
                 print(self.symbol_table)
                 return
 
-    def find_addr(self, lexeme):
-        if lexeme in self.symbol_table.keys():
-            return self.symbol_table[lexeme][1]
-        else:
-            self.symbol_table[lexeme] = [self.cur_type, self.addr, 1]
-            self.addr += 4
-            return self.symbol_table[lexeme][1]
+    def find_addr(self, lexeme, insert=False):
+        for record in self.symbol_table[::-1]:
+            if record[0] == lexeme:
+                return record[2]
+
+        if not insert:
+            return None
+        self.symbol_table.append([lexeme, self.cur_type, self.temp_addr, 1, self.cur_scope])
+        self.temp_addr += 4
+        return self.symbol_table[-1][2]
 
     def print_output(self):
         for i in range(len(self.PB)):
